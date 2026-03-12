55c9d1869c177f2abf391f1b493b57d66a2a5256
Carmen Cunha <carmencunha29@gmail.com>
2026-03-12 21:15:44 +0000

    Configurando ALLOWED_HOSTS para o Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-57oqarc%hm+a!ip8^t&qfml3sq$vyr^1h%$0k-bu)=4r@q_!&&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luz-da-palavra.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
