cf0fa3e42260b4d9c172416065a6f1ece96775e3
Carmen Cunha <carmencunha29@gmail.com>
2026-03-12 22:01:03 +0000

    Finalizando configuracoes de seguranca

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57oqarc%hm+a!ip8^t&qfml3sq$vyr^1h%$0k-bu)=4r@q_!&&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['luz-da-palavra.onrender.com', 'localhost', '127.0.0.1']
 
